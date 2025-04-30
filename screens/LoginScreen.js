c52c0d3fe5bbef48a592fc4807db37005adc5630
Jaydan <139529445+TheWhyz@users.noreply.github.com>
2025-04-29 17:21:10 -0700

    Revert "Sign-in on enter and UI fixes"

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,17 +1,7 @@
-import { useState } from "react";
-import {
-  View,
-  TextInput,
-  Text,
-  Alert,
-  TouchableOpacity,
-  StyleSheet,
-  ScrollView,
-  KeyboardAvoidingView,
-  Platform,
-} from "react-native";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../firebaseConfig";
+import { useState } from 'react'
+import { View, TextInput, Text, Alert, TouchableOpacity, StyleSheet, ScrollView, KeyboardAvoidingView, Platform } from 'react-native'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '../firebaseConfig'
 
 /**
  * LoginScreen Component
@@ -23,26 +13,23 @@ import { auth } from "../firebaseConfig";
  * @param {Object} navigation - React Navigation prop used to navigate between screens.
  */
 const LoginScreen = ({ navigation }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [showPassword, setShowPassword] = useState(false)
 
   // Login function
   const handleLogin = async () => {
     try {
-      await signInWithEmailAndPassword(auth, email, password);
-      navigation.navigate("Home");
+      await signInWithEmailAndPassword(auth, email, password)
+      navigation.navigate('Home')
     } catch (error) {
-      Alert.alert(
-        "Login failed",
-        "Invalid email or password. Please try again."
-      );
+      Alert.alert('Login failed', 'Invalid email or password. Please try again.')
     }
-  };
+  }
 
   return (
     <KeyboardAvoidingView
-      behavior={Platform.OS === "ios" ? "padding" : "height"} // Adjust behavior for iOS/Android
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'} // Adjust behavior for iOS/Android
       style={styles.container}
     >
       <ScrollView contentContainerStyle={styles.scrollContainer}>
@@ -55,9 +42,9 @@ const LoginScreen = ({ navigation }) => {
         <TextInput
           value={email}
           onChangeText={setEmail}
-          placeholder="Enter your email"
-          keyboardType="email-address"
-          autoCapitalize="none"
+          placeholder='Enter your email'
+          keyboardType='email-address'
+          autoCapitalize='none'
           style={styles.input}
         />
 
@@ -66,20 +53,16 @@ const LoginScreen = ({ navigation }) => {
           <TextInput
             value={password}
             onChangeText={setPassword}
-            placeholder="Enter your password"
+            placeholder='Enter your password'
             secureTextEntry={!showPassword} // Toggle password visibility
             style={styles.passwordInput}
-            onSubmitEditing={handleLogin} // Trigger login when "Enter" is pressed
-            returnKeyType="done" // Adjust the keyboard "Enter" button appearance
           />
           <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
-            <Text style={styles.showPassword}>
-              {showPassword ? "HIDE" : "SHOW"}
-            </Text>
+            <Text style={styles.showPassword}>{showPassword ? 'HIDE' : 'SHOW'}</Text>
           </TouchableOpacity>
         </View>
 
-        <TouchableOpacity onPress={() => navigation.navigate("SignUp")}>
+        <TouchableOpacity onPress={() => navigation.navigate('SignUp')}>
           <Text style={styles.signupText}>Don’t have an account?</Text>
         </TouchableOpacity>
 
@@ -87,93 +70,93 @@ const LoginScreen = ({ navigation }) => {
           <Text style={styles.buttonText}>Sign In</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity onPress={() => navigation.navigate("ResetPassword")}>
+        <TouchableOpacity onPress={() => navigation.navigate('ResetPassword')}>
           <Text style={styles.forgotText}>Forgot Password?</Text>
         </TouchableOpacity>
       </ScrollView>
     </KeyboardAvoidingView>
-  );
-};
+  )
+}
 
 // Styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff'
   },
   scrollContainer: {
     flexGrow: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     paddingHorizontal: 20,
-    paddingBottom: 30, // Added space to the bottom to avoid cover by the keyboard
+    paddingBottom: 30 // Added space to the bottom to avoid cover by the keyboard
   },
   header: {
-    width: "100%",
-    backgroundColor: "#CC0000",
+    width: '100%',
+    backgroundColor: '#CC0000',
     paddingVertical: 20,
-    alignItems: "center",
-    marginBottom: 30,
+    alignItems: 'center',
+    marginBottom: 30
   },
   headerText: {
-    color: "#fff",
+    color: '#fff',
     fontSize: 24,
-    fontWeight: "bold",
-    textShadowColor: "black",
+    fontWeight: 'bold',
+    textShadowColor: 'black',
     textShadowOffset: { width: 2, height: 2 },
-    textShadowRadius: 3,
+    textShadowRadius: 3
   },
   label: {
-    alignSelf: "flex-start",
+    alignSelf: 'flex-start',
     fontSize: 16,
-    fontWeight: "bold",
-    marginBottom: 5,
+    fontWeight: 'bold',
+    marginBottom: 5
   },
   input: {
-    width: "100%",
-    backgroundColor: "#d3d3d3",
+    width: '100%',
+    backgroundColor: '#d3d3d3',
     padding: 10,
     borderRadius: 5,
-    marginBottom: 15,
+    marginBottom: 15
   },
   passwordContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    width: "100%",
-    backgroundColor: "#d3d3d3",
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+    backgroundColor: '#d3d3d3',
     borderRadius: 5,
-    paddingHorizontal: 10,
+    paddingHorizontal: 10
   },
   passwordInput: {
     flex: 1, // Makes the input take up most of the space
-    paddingVertical: 10,
+    paddingVertical: 10
   },
   showPassword: {
-    color: "#CC0000",
-    fontWeight: "bold",
+    color: '#CC0000',
+    fontWeight: 'bold'
   },
   signupText: {
-    color: "#6495ED",
-    marginBottom: 20,
+    color: '#6495ED',
+    marginBottom: 20
   },
   button: {
-    width: "100%",
-    backgroundColor: "#CC0000",
+    width: '100%',
+    backgroundColor: '#CC0000',
     paddingVertical: 15,
-    alignItems: "center",
+    alignItems: 'center',
     borderRadius: 5,
-    marginBottom: 10,
+    marginBottom: 10
   },
   buttonText: {
-    color: "#fff",
+    color: '#fff',
     fontSize: 16,
-    fontWeight: "bold",
+    fontWeight: 'bold'
   },
   forgotText: {
-    color: "red",
+    color: 'red',
     fontSize: 14,
-    marginTop: 10,
-  },
-});
+    marginTop: 10
+  }
+})
 
-export default LoginScreen;
+export default LoginScreen
