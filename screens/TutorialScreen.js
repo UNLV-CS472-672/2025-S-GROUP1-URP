93342f23824a280cffae2a9b3fab7a9b659e0bb1
Jaydan <janiesco63@gmail.com>
2025-04-29 21:16:33 -0700

    Added logo to first screen of the tutorial

--- screens/TutorialScreen.js
+++ screens/TutorialScreen.js
@@ -12,12 +12,14 @@ export default function TutorialScreen({ navigation }) {
                     backgroundColor: '#fff',
                     image: (
                         <Image
-                            source={{ uri: 'https://cdn-icons-png.flaticon.com/512/684/684908.png' }}
-                            style={{ width: 200, height: 200 }}
+                            source={require('../assets/tutorial/URPLogo.png')}
+                            style={{ width: 500, height: 500, margin: 0 }}
                         />
                     ),
                     title: 'Welcome to URP!',
                     subtitle: 'Reserve your UNLV parking spot easily through our app.',
+                    titleStyles: { marginTop: -220 }, 
+                    subTitleStyles: { marginTop: -180 },
                 },
                 {
                     backgroundColor: '#f9f9f9',
@@ -29,6 +31,8 @@ export default function TutorialScreen({ navigation }) {
                     ),
                     title: 'Find Parking',
                     subtitle: 'Explore available spots in real-time across garages.',
+                    titleStyles: { marginTop: -100 },
+                    subTitleStyles: { marginTop: -50 },
                 },
                 {
                     backgroundColor: '#fff',
@@ -40,6 +44,8 @@ export default function TutorialScreen({ navigation }) {
                     ),
                     title: 'Need Help?',
                     subtitle: 'Tap the Help button any time to get guidance or support.',
+                    titleStyles: { marginTop: -100 },
+                    subTitleStyles: { marginTop: -50 },
                 },
                 {
                     backgroundColor: '#e6ffe6',
@@ -51,6 +57,8 @@ export default function TutorialScreen({ navigation }) {
                     ),
                     title: 'You’re All Set!',
                     subtitle: 'Let’s go find your perfect parking spot.',
+                    titleStyles: { marginTop: -100 },
+                    subTitleStyles: { marginTop: -50 },
                 },
             ]}
         />
